04928b82e4dcdfaa45cbda78d48ef5c9759643c8
M N Hida <hidactive@gmail.com>
2023-05-25 23:56:12 +0700

    Update hda_asn1Parser.php

--- hda_asn1Parser.php
+++ hda_asn1Parser.php
@@ -1,4 +1,3 @@
-<pre>
 <?php
 //============================================================+
 // File name   : hda_asn1Parser.php
@@ -16,102 +15,107 @@
 
 
 
-// unrecursive function
+	/**
+	 * parse asn.1 to array
+	 * to be called from $this->parse_recursive() function
+	 * @param string $hex asn.1 hex form
+	 * @return array asn.1 structure
+	 * @protected
+	 */
 function hda_asn1parse($hex) {
-	  if(!@ctype_xdigit($hex) || @strlen($hex)%2!=0) {
-		  return false;
-	  }
-	  $stop = false;
-	  while($stop == false) {
-      $asn1_type = substr($hex, 0, 2);
-      $tlv_tagLength = hexdec(substr($hex, 2, 2));
-      if($tlv_tagLength > 127) {
-        $tlv_lengthLength = $tlv_tagLength-128;
-        $tlv_valueLength = substr($hex, 4, ($tlv_lengthLength*2));
-      } else {
-        $tlv_lengthLength = 0;
-        $tlv_valueLength = substr($hex, 2, 2+($tlv_lengthLength*2));
-      }
-      if($tlv_lengthLength >4) { // limit tlv_lengthLength to FFFF
-        return false;
-      }
-      //if($tlv_valueLength > intval(str_repeat('9',14))) { // limit to 99.999.999.999.999
-        //return false;
-      //}
-      $tlv_valueLength = hexdec($tlv_valueLength);
-      
-      $totalTlLength = 2+2+($tlv_lengthLength*2);
-      $reduction = 2+2+($tlv_lengthLength*2)+($tlv_valueLength*2);
-      $tlv_value = substr($hex, $totalTlLength, $tlv_valueLength*2);
-      $remain = substr($hex, $totalTlLength+($tlv_valueLength*2));
-      $newhexdump = substr($hex, 0, $totalTlLength+($tlv_valueLength*2));
-      
-      $result[] = array(
-                        //'tlv_tagLength'=>strlen(dechex($tlv_tagLength))%2==0?dechex($tlv_tagLength):'0'.dechex($tlv_tagLength),
-                        //'tlv_lengthLength'=>$tlv_lengthLength,
-                        'tlv_valueLength'=>$tlv_valueLength,
-                        //'reduction'=>$reduction,
-                        //'hex'=>$hex,
-                        'newhexdump'=>$newhexdump,
-                        'typ'=>$asn1_type,
-                        'tlv_value'=>$tlv_value
-                        //'remain'=>$remain
-                        );
+	if(!@ctype_xdigit($hex) || @strlen($hex)%2!=0) {
+		$this->errorMsg = "input not hex string!.";
+		return false;
+	}
+	$stop = false;
+	while($stop == false) {
+		$asn1_type = substr($hex, 0, 2);
+		$tlv_tagLength = hexdec(substr($hex, 2, 2));
+		if($tlv_tagLength > 127) {
+			$tlv_lengthLength = $tlv_tagLength-128;
+			$tlv_valueLength = substr($hex, 4, ($tlv_lengthLength*2));
+		} else {
+			$tlv_lengthLength = 0;
+			$tlv_valueLength = substr($hex, 2, 2+($tlv_lengthLength*2));
+		}
+		if($tlv_lengthLength >4) { // limit tlv_lengthLength to FFFF
+			return false;
+		}
+		$tlv_valueLength = hexdec($tlv_valueLength);
+		
+		$totalTlLength = 2+2+($tlv_lengthLength*2);
+		$reduction = 2+2+($tlv_lengthLength*2)+($tlv_valueLength*2);
+		$tlv_value = substr($hex, $totalTlLength, $tlv_valueLength*2);
+		$remain = substr($hex, $totalTlLength+($tlv_valueLength*2));
+		$newhexdump = substr($hex, 0, $totalTlLength+($tlv_valueLength*2));
+		
+		$result[] = array(
+						'tlv_tagLength'=>strlen(dechex($tlv_tagLength))%2==0?dechex($tlv_tagLength):'0'.dechex($tlv_tagLength),
+						'tlv_lengthLength'=>$tlv_lengthLength,
+						'tlv_valueLength'=>$tlv_valueLength,
+						'newhexdump'=>$newhexdump,
+						'typ'=>$asn1_type,
+						'tlv_value'=>$tlv_value
+						);
 
-      //if($remain == '' && $tlv_value == '' && $asn1_type != '05') { // if remains string was empty & contents also empty, function return FALSE
-      if($remain == '') { // if remains string was empty & contents also empty, function return FALSE
-        $stop = true;
-      } else {
-        $hex = $remain;
-      }
-	  }
-	  return $result;
+		if($remain == '') { // if remains string was empty & contents also empty, function return FALSE
+			$stop = true;
+		} else {
+			$hex = $remain;
+		}
+	}
+	return $result;
 }
 
 
 
-// Main function
+	/**
+	 * parse asn.1 to array recursively
+	 * @param string $hex asn.1 hex form
+	 * @param int $maxDepth maximum parsing depth
+	 * @return array asn.1 structure recursively to specific depth
+	 * @public
+	 */
 function hda_asn1parse_recursive($hex, $maxDepth=5) {
-  $result = array();
-  $asn1parse_array = hda_asn1parse($hex);
-  static $currentDepth = 0;
-  if($asn1parse_array) {
-    foreach($asn1parse_array as $ff){
-      $k = $ff['typ'];
-      $v = $ff['tlv_value'];
-      $info['depth']=$currentDepth;
-      $info['hexdump']=$ff['newhexdump'];
-      $info['type'] = $k;  
-      $info['value_hex'] = $v;  
-      if(($currentDepth <= $maxDepth)) {
-        if($info['type'] == '06') {
+	$result = array();
+	$info = array();
+	$parse_recursive = array();
+	$asn1parse_array = $this->parse($hex);
+	static $currentDepth = 0;
+	if($asn1parse_array) {
+		foreach($asn1parse_array as $ff){
+			$k = $ff['typ'];
+			$v = $ff['tlv_value'];
+			$info['depth']=$currentDepth;
+			$info['hexdump']=$ff['newhexdump'];
+			$info['type'] = $k;  
+			$info['value_hex'] = $v;  
+			if(($currentDepth <= $maxDepth)) {
+				if($k == '06') {
 
-        } else if($info['type'] == '13' ||
-                   $info['type'] == '18'
-                   ) {
-          $info['value'] = hex2bin($info['value_hex']);
-        } else if(
-                   $info['type'] == '03' ||
-                   $info['type'] == '02'
-                   ) {
-          $info['value'] = $info['value_hex'];
-        } else if(
-                   $info['type'] == '05'
-                   ) {
+				} else if($k == '13' || $k == '18') {
+					$info['value'] = hex2bin($info['value_hex']);
+				} else if($k == '03' || $k == '02') {
+					$info['value'] = $v;
+				} else if($k == '05') {
 
-        } else {
-          $currentDepth++;
-          $info['value'] = hda_asn1parse_recursive($v, $maxDepth); 
-          $currentDepth--;
-        }
-        $result[] = $info;
-      }
-    }
-  } else {
-    //unset($info['value']);
-    $result = false;
-  }
-  return $result;
+				} else {
+					$currentDepth++;
+					$parse_recursive = $this->parse_recursive($v, $maxDepth); 
+					$currentDepth--;
+				}
+				if($parse_recursive) {
+					$result[] = array_merge($info, $parse_recursive);
+				} else {
+					$result[] = $info;
+				}
+				unset($info['value']);
+			}
+		}
+	} else {
+		$result = false;
+	}
+	return $result;
 }
 
 ?>
