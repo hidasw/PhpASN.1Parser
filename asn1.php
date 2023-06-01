2dde86261cd2dfc6d1b5651430f8b8e270449c53
M N Hida <hidactive@gmail.com>
2023-06-02 01:43:51 +0700

    update

--- asn1.php
+++ asn1.php
@@ -3,7 +3,7 @@
 // File name   : hda_asn1Parser.php
 // Version     : 1.0.1
 // Begin       : 26/03/2009
-// Last Update : 01/06/2023
+// Last Update : 02/06/2023
 // Author      : Hida - https://github.com/hidasw
 // License     : GPL-3.0 license
 // -------------------------------------------------------------------
@@ -15,6 +15,45 @@
 
 
 class asn1 {
+	// =====Begin ASN.1 Parser section=====
+	/**
+	 * get asn.1 type tag name
+	 * @param string $id hex asn.1 type tag
+	 * @return string asn.1 tag name
+	 * @protected
+	 */
+	protected static function type($id) {
+		$asn1_Types = array(
+		"00" => "ASN1_EOC",
+		"01" => "ASN1_BOOLEAN",
+		"02" => "ASN1_INTEGER",
+		"03" => "ASN1_BIT_STRING",
+		"04" => "ASN1_OCTET_STRING",
+		"05" => "ASN1_NULL",
+		"06" => "ASN1_OBJECT",
+		"07" => "ASN1_OBJECT_DESCRIPTOR",
+		"08" => "ASN1_EXTERNAL",
+		"09" => "ASN1_REAL",
+		"0a" => "ASN1_ENUMERATED",
+		"0c" => "ASN1_UTF8STRING",
+		"30" => "ASN1_SEQUENCE",
+		"31" => "ASN1_SET",
+		"12" => "ASN1_NUMERICSTRING",
+		"13" => "ASN1_PRINTABLESTRING",
+		"14" => "ASN1_T61STRING",
+		"15" => "ASN1_VIDEOTEXSTRING",
+		"16" => "ASN1_IA5STRING",
+		"17" => "ASN1_UTCTIME",
+		"18" => "ASN1_GENERALIZEDTIME",
+		"19" => "ASN1_GRAPHICSTRING",
+		"1a" => "ASN1_VISIBLESTRING",
+		"1b" => "ASN1_GENERALSTRING",
+		"1c" => "ASN1_UNIVERSALSTRING",
+		"1d" => "ASN1_BMPSTRING"
+		);
+		return array_key_exists($id,$asn1_Types)?$asn1_Types[$id]:$id;
+	}
+
 	/**
 	 * parse asn.1 to array
 	 * to be called from parse() function
@@ -24,7 +63,7 @@ class asn1 {
 	 */
 	protected static function oneParse($hex) {
 		if(!@ctype_xdigit($hex) || @strlen($hex)%2!=0) {
-			$this->errorMsg = "input not hex string!.";
+			echo "input:\"$hex\" not hex string!.\n";
 			return false;
 		}
 		$stop = false;
@@ -76,30 +115,32 @@ class asn1 {
 	 */
 	public static function parse($hex, $maxDepth=5) {
 		$result = array();
-		$info = array();
-		$parse_recursive = array();
-		$asn1parse_array = self::oneParse($hex);
 		static $currentDepth = 0;
-		if($asn1parse_array) {
+		if($asn1parse_array = self::oneParse($hex)) {
 			foreach($asn1parse_array as $ff){
+				$parse_recursive = false;
+				unset($info);
 				$k = $ff['typ'];
 				$v = $ff['tlv_value'];
 				$info['depth']=$currentDepth;
 				$info['hexdump']=$ff['newhexdump'];
 				$info['type'] = $k;  
+				$info['typeName'] = self::type($k);  
 				$info['value_hex'] = $v;  
 				if(($currentDepth <= $maxDepth)) {
 					if($k == '06') {
-
+						
 					} else if($k == '13' || $k == '18') {
 						$info['value'] = hex2bin($info['value_hex']);
-					} else if($k == '03' || $k == '02') {
+					} else if($k == '03' || $k == '02' || $k == 'a04') {
 						$info['value'] = $v;
 					} else if($k == '05') {
+						
+					} else if($k == '01') {
 
 					} else {
 						$currentDepth++;
-						$parse_recursive = self::parse($v, $maxDepth); 
+						$parse_recursive = self::parse($v, $maxDepth);
 						$currentDepth--;
 					}
 					if($parse_recursive) {
@@ -107,13 +148,222 @@ class asn1 {
 					} else {
 						$result[] = $info;
 					}
-					unset($info['value']);
 				}
 			}
-		} else {
-			$result = false;
 		}
 		return $result;
 	}
+	// =====End ASN.1 Parser section=====
+
+	// =====Begin ASN.1 Builder section=====
+	/**
+	 * create asn.1 TLV tag length, length length and value length
+	 * to be called from asn.1 builder functions
+	 * @param string $str string value of asn.1
+	 * @return string hex of asn.1 TLV tag length
+	 * @protected
+	 */
+	protected static function asn1_header($str) {
+		$len = strlen($str)/2;
+		$ret = dechex($len);
+		if(strlen($ret)%2 != 0) {
+			$ret = "0$ret";
+		}
+
+		$headerLength = strlen($ret)/2;
+		if($len > 127) {
+			$ret = "8".$headerLength.$ret;
+		}
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 SEQUENCE
+	 * @param string $hex hex value of asn.1 SEQUENCE
+	 * @return tring hex of asn.1 SEQUENCE tag with value
+	 * @public
+	 */
+	public static function SEQUENCE($hex) {
+		$ret = "30".self::asn1_header($hex).$hex;
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 OCTET
+	 * @param string $hex hex value of asn.1 OCTET
+	 * @return string hex of asn.1 OCTET tag with value
+	 * @public
+	 */
+	public static function OCTET($hex)  {
+		$ret = "04".self::asn1_header($hex).$hex;
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 OBJECT
+	 * @param string $hex hex value of asn.1 OBJECT
+	 * @return string hex of asn.1 OBJECT tag with value
+	 * @public
+	 */
+	public static function OBJECT($hex)  {
+		$ret = "06".self::asn1_header($hex).$hex;
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 BITString
+	 * @param string $hex hex value of asn.1 BITString
+	 * @return string hex of asn.1 BITString tag with value
+	 * @public
+	 */
+	public static function BITSTR($hex)  {
+		$ret = "03".self::asn1_header($hex).$hex;
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 PRINTABLEString tag
+	 * @param string $str string value of asn.1 PRINTABLEString
+	 * @return string hex of asn.1 PRINTABLEString tag with value
+	 * @public
+	 */
+	public static function PRINTABLESTR($str)  {
+		$ret = "13".self::asn1_header(str_repeat($str, 2)).bin2hex($str);
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 INTEGER
+	 * @param string $int number value of asn.1 INTEGER
+	 * @return string hex of asn.1 INTEGER tag with value
+	 * @public
+	 */
+	public static function INTEGER($int)  {
+		if(strlen($int)%2 != 0)  {
+		$int = "0$int";
+		}
+		$int = "$int";
+		$ret = "02".self::asn1_header($int).$int;
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 SET tag
+	 * @param string $hex hex value of asn.1 SET
+	 * @return string hex of asn.1 SET with value
+	 * @public
+	 */
+	public static function SET($hex)  {
+		$ret = "31".self::asn1_header($hex).$hex;
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 EXPLICIT
+	 * @param string $num value of asn.1 EXPLICIT number
+	 * @param string $hex value of asn.1 EXPLICIT
+	 * @return string hex of asn.1 EXPLICIT with value
+	 * @public
+	 */
+	public static function EXPLICIT($num, $hex)  {
+		$ret = "a$num".self::asn1_header($hex).$hex;
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 IMPLICIT
+	 * @param integer $num value of asn.1 IMPLICIT
+	 * @return string hex of asn.1 IMPLICIT tag with value
+	 * @public
+	 */
+	public static function IMPLICIT($num="0")  {
+		if(strlen($num)%2 != 0)  {
+		$num = "0$num";
+		}
+		$ret = "80$num";
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 UTCTIME
+	 * @param string $time string value of asn.1 UTCTIME date("ymdHis")
+	 * @return string hex of asn.1 UTCTIME tag with value
+	 * @public
+	 */
+	public static function UTCTIME($time) {
+		$ret = "170d".bin2hex($time)."5a";
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 GENERALIZEDTIME
+	 * @param string $time string value of asn.1 GENERALIZEDTIME date("YmdHis")
+	 * @return string hex of asn.1 GENERALIZEDTIME tag with value
+	 * @public
+	 */
+	public static function GENERALIZEDTIME($time)  {
+		$ret = "180f".bin2hex($time)."5a";
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 UTF8String
+	 * @param string $str string value of asn.1 UTF8String
+	 * @return string hex of asn.1 UTF8String tag with value
+	 * @public
+	 */
+	public static function UTF8STR($str) {
+		$ret = "0c".self::asn1_header(bin2hex($str)).bin2hex($str);
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 IA5String
+	 * @param string $str string value of asn.1 IA5String
+	 * @return string hex of asn.1 IA5String tag with value
+	 * @public
+	 */
+	public static function IA5STR($str) {
+		$ret = "16".self::asn1_header(bin2hex($str)).bin2hex($str);
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 VISIBLEString
+	 * @param string $str string value of asn.1 VISIBLEString
+	 * @return string hex of asn.1 VISIBLEString tag with value
+	 * @public
+	 */
+	public static function VISIBLESTR($str) {
+		$ret = "1a".self::asn1_header(bin2hex($str)).bin2hex($str);
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 T61String
+	 * @param string $str string value of asn.1 T61String
+	 * @return string hex of asn.1 T61String tag with value
+	 * @public
+	 */
+	public static function T61STR($str) {
+		$ret = "14".self::asn1_header(bin2hex($str)).bin2hex($str);
+		return $ret;
+	}
+
+	/**
+	 * Create asn.1 custom tag
+	 * @param string hex value of asn.1 custom tag
+	 * @return string hex of asn.1 custom tag with value
+	 * @public
+	 */
+	public static function OTHER($id, $hex, $chr = false) {
+		$str = $hex;
+		if($chr != false) {
+		$str = bin2hex($hex);
+		}
+		$ret = "$id".self::asn1_header($str).$str;
+		return $ret;
+	}
+	// =====End ASN.1 Builder section=====
 }
 ?>
--- test/index.php
+++ test/index.php
@@ -5,12 +5,22 @@ ob_start(); // just for print line number on left side page
 
 require('../asn1.php');
 
+// Create ASN.1 Form
+$asn1Hex = asn1::sequence(
+							asn1::generalizedtime('20230102121314').
+							asn1::printablestr('Hello world.')
+							);
+$asn1Der = hex2bin($asn1Hex); // can be read by openssl asn1parse -i -inform der
+$asn1Base64 = chunk_split(base64_encode($asn1Der), 65); // can be read by openssl asn1parse -i
+echo "ASN.1 Hex Form :".$asn1Hex."\n";
+echo "ASN.1 Base64   :".$asn1Base64."\n";
+
 // Parse asn.1 (x509 binary/der certificate)
 $derfile = file_get_contents('Local Root CA.cer');
 // Conver to hex
 $hexform = bin2hex($derfile);
-// Parsing with depth 5
-$result = asn1::parse($hexform,5);
+// Parsing with depth 9
+$result = asn1::parse($hexform,9);
 // View result
 print_r($result);
 
